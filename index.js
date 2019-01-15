a40f0ed2f5d0a1deb0d6481db340e97f75c04d03
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-01-15 02:14:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const driver = {name: undefined, address: undefined};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-   driver[key] = value;
-   return driver;
+   new_driver = driver;
+   new_driver[key] = value;
+   return new_driver;
 }
